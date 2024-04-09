4ab379d13ae9f6d64deb987ddfc4ce72bbc09d75
imcoa1 ISCTE <79607955+inesconsolado-ISCTE@users.noreply.github.com>
2024-04-09 16:44:56 +0100

    defini funções teste; desenvolvi umas funções mas muito pouco;

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,2 +1,39 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+
 class Test {
+
+        //criar documento XML para testar
+
+        @Test
+        fun addEntity(){}
+
+        @Test
+        fun removeEntity(){}
+
+        @Test
+        fun addAttribute(){}
+
+        @Test
+        fun removeAttribute(){}
+
+        @Test
+        fun changeAttribute(){}
+
+        //aceder a uma entidade é suposto ter uma função de teste? Se calhar devolver a lista dos seus filhos
+
+        @Test
+        fun addAttributeGlobally(){}
+
+        @Test
+        fun renameEntityGlobally(){}
+
+        @Test
+        fun removeEntityGlobally(){}
+
+        @Test
+        fun removeAttributeGlobally(){}
+
+
 }
