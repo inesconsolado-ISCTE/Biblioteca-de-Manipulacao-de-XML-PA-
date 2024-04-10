5ec04c06b00c09fe1bfce2d89aca9883116a9456
marianaiscte <125377466+marianaiscte@users.noreply.github.com>
2024-04-10 17:47:16 +0100

    Update XML.kt

--- src/main/kotlin/XML.kt
+++ src/main/kotlin/XML.kt
@@ -1,28 +1,53 @@
+//VAL É IMUTÁVEL
+//VAR É MUTÁVEL
+
 sealed interface XMLChild{
     val value: String
-    val parent: Tag?
+    val parent: XMLParent?
+}
 
-    //1. remover tag e texto
+sealed interface XMLParent{
+    var children: MutableList<XMLChild>
+
+    fun addChild(child: XMLChild){
+        children.add(child)
+    }
+
+    fun removeChild(child: XMLChild) {
+        children.remove(child)
+        if (child is XMLParent) {
+            child.children.forEach {
+                removeChild(it)
+            }
+        }
+    }
+
 }
 
 //DUVIDA: Onde é que escrevemos as coisas no xml em si? Tipo com as < > e isso tudo
 
-data class Document(val encode: String, val value: String){
 
-    var children: MutableList<XMLChild> = mutableListOf()
+data class Document(val encode: String, val version: String): XMLParent{
 
-    //pôr aquela primeira linha de um xml
+    override var children: MutableList<XMLChild> = mutableListOf()
+
+    init {
+        val xmlDeclarationText = "?xml version=\"$version\" encoding=\"$encode\"?"
+    }
 
     //6. Add atributo globalmente e o resto
     fun addAttribute(tag: Tag, name: String, value: String){}
 
-    //4. val prettyPrint:
+    //4. prettyPrint: escrever tudo no ficheiro e pôr bonito
+    fun prettyPrint(){
+    }
+
 
 }
 
-data class Tag(override val value: String, override val parent: Tag? = null): XMLChild{
+data class Tag(override val value: String, override val parent: Tag? = null): XMLChild, XMLParent {
 
-    var children: MutableList<XMLChild> = mutableListOf()
+    override var children: MutableList<XMLChild> = mutableListOf()
     var attributes: MutableList<Attribute> = mutableListOf()
 
     init {
@@ -30,36 +55,26 @@ data class Tag(override val value: String, override val parent: Tag? = null): XM
     }
 
     //2. Add, remover e alterar atributos em entidades
-    // é suposto nestas funções "escrever" no xml ou isso é o pretty print?
-    fun addAttribute(atr: Attribute){
-        if(attributes.any {it.name == atr.name}){  //verifica se já existe um atributo com este nome
-
-        }
-        else attributes.add(atr)
-    //tbm pode receber uma lista de atributos em vez de ser só um, para ser mais eficiente?
-    // E se quiser adicionar só um é uma lista de um?
+    fun addAttribute(atr: Attribute) {
+        if (attributes.any { it.name.equals(atr.name) }) {  //verifica se já existe um atributo com este nome
+            //lançar excepção!! JÁ TEM ESSE NOME
+        } else attributes.add(atr)
     }
 
 
     //Usar o quê para procurar o atributo na lista? O nome? Então tem de ser único
-    fun removeAttribute(atr: Attribute){
+    fun removeAttribute(atr: Attribute) {
         attributes.remove(atr)
     }
 
-    fun changeAttribute(atr: Attribute, newvalue: String){
-        val attrToChange = attributes.find {it.name == atr.name}
+    fun changeAttribute(atr: Attribute, newvalue: String) {
+        val attrToChange = attributes.find { it.name == atr.name }
         if (attrToChange != null) {
             attrToChange.value = newvalue
         }
     }
-
-    fun addChild(child: XMLChild){
-        children.add(child)
-    }
-
-
 }
-
+    
 data class Attribute(var name: String, var value: String){
 
 }
