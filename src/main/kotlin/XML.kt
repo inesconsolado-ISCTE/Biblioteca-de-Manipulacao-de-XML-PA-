bf55d7cefaf95895bce28e10d2bb209a10f876a2
imcoa1 ISCTE <79607955+inesconsolado-ISCTE@users.noreply.github.com>
2024-04-12 12:41:24 +0100

    visitor feito mas por testar; tentativa da função 7

--- src/main/kotlin/XML.kt
+++ src/main/kotlin/XML.kt
@@ -7,7 +7,20 @@ sealed interface XMLChild{
 }
 
 sealed interface XMLParent{
-    var children: MutableList<XMLChild>
+    val children: MutableList<XMLChild>
+
+    /*Implementação da interface visitor
+    XMLParent é o que está a ser visitado (daí implementarmos aqui): o documento (e consequentemente tags)
+    Fazer já um accept que "faz coisas" aqui pq vai ser usado no document e na tag da mesma maneira?
+     */
+    fun accept(visitor: (XMLParent) -> Boolean){
+        if (visitor(this)) {
+            this.children.forEach {
+                if(it is XMLParent)  //sem este if teriamos de ter uma função accept tbm no XMLChild
+                    it.accept(visitor)
+            }
+        }
+    }
 
     fun addChild(child: XMLChild){
         children.add(child)
@@ -24,38 +37,55 @@ sealed interface XMLParent{
 
 }
 
-//DUVIDA: Onde é que escrevemos as coisas no xml em si? Tipo com as < > e isso tudo
-
+/*Notinhas:
+- DUVIDA: Onde é que escrevemos as coisas no xml em si? Tipo com as < > e isso tudo
+- só pode haver uma root tag
+- Deviamos verificar se um child pode ou não ser adicionado: se já ouver text não podemos acrescentar uma tag como child
+*/
 
 data class Document(val encode: String, val version: String): XMLParent{
 
-    override var children: MutableList<XMLChild> = mutableListOf()
+    override val children: MutableList<XMLChild> = mutableListOf()
 
     init {
         val xmlDeclarationText = "?xml version=\"$version\" encoding=\"$encode\"?"
     }
 
     //6. Add atributo globalmente e o resto
-    fun addAttribute(tag: Tag, name: String, value: String){}
+    fun addAttribute(tag: Tag, name: String, value: String){
+
+    }
+
+    //7.  renomeação de entidades globalmente ao documento (fornecendo nome antigo e nome novo)
+    fun renameEntityGlobally(oldname: String, newname: String){
+        this.accept {
+            if((it is Tag) && it.value.equals(oldname)) {
+                    it.value = newname
+                }
+            false
+        }
+    }
 
     //4. prettyPrint: escrever tudo no ficheiro e pôr bonito
     fun prettyPrint(){
     }
 
 
+
 }
 
-data class Tag(override val value: String, override val parent: Tag? = null): XMLChild, XMLParent {
-
-    override var children: MutableList<XMLChild> = mutableListOf()
-    var attributes: MutableList<Attribute> = mutableListOf()
+data class Tag(override var value: String, override val parent: Tag? = null): XMLChild, XMLParent {
+    //com val em vez de var não se pode mudar a lista toda pô-la toda a null, mas pode se fazer alterações na mesma pq é mutable
+    override val children: MutableList<XMLChild> = mutableListOf()
+    private val attributes: MutableList<Attribute> = mutableListOf()
 
     init {
         parent?.children?.add(this)
     }
 
     //2. Add, remover e alterar atributos em entidades
-    fun addAttribute(atr: Attribute) {
+    //Podiamos usar ignoreCase = true
+    /*fun addAttribute(atr: Attribute) {
         if (attributes.any { it.name.equals(atr.name) }) {  //verifica se já existe um atributo com este nome
             //lançar excepção!! JÁ TEM ESSE NOME
         } else attributes.add(atr)
@@ -68,11 +98,11 @@ data class Tag(override val value: String, override val parent: Tag? = null): XM
     }
 
     fun changeAttribute(atr: Attribute, newvalue: String) {
-        val attrToChange = attributes.find { it.name == atr.name }
+        val attrToChange = attributes.find { it.name.equals(atr.name) }
         if (attrToChange != null) {
             attrToChange.value = newvalue
         }
-    }
+    }*/
 }
     
 data class Attribute(var name: String, var value: String){
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.io.File
 
 class Test {
 
